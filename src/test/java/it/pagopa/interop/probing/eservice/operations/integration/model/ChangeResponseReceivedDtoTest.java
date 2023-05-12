f4598598d59569329785c76b81debce9d068c67b
angelaantoniaceaa <107028078+angelaantoniaceaa@users.noreply.github.com>
2023-05-12 14:19:10 +0200

    Merge feature/IESP-180 (#4)

---
+++ src/test/java/it/pagopa/interop/probing/eservice/operations/integration/model/ChangeResponseReceivedDtoTest.java
@@ -0,0 +1,38 @@
+package it.pagopa.interop.probing.eservice.operations.integration.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import it.pagopa.interop.probing.response.updater.dto.impl.ChangeResponseReceivedDto;
+import it.pagopa.interop.probing.response.updater.util.EserviceStatus;
+
+class ChangeResponseReceivedDtoTest {
+
+  Long eserviceRecordId = 123L;
+  OffsetDateTime responseReceived = OffsetDateTime.now(ZoneOffset.UTC);
+  EserviceStatus status = EserviceStatus.OK;
+
+  private ChangeResponseReceivedDto changeResponseReceivedDto;
+
+  @BeforeEach
+  void setUp() {
+    changeResponseReceivedDto =
+        ChangeResponseReceivedDto.builder().eserviceRecordId(eserviceRecordId)
+            .responseReceived(responseReceived).status(status).build();
+  }
+
+  @Test
+  @DisplayName("given valid input data ChangeResponseReceivedDto object is created")
+  void testBuilder_whenGivenValidInputData_thenDtoCreatedWithCorrectValues() {
+    assertNotNull(changeResponseReceivedDto);
+    assertEquals(eserviceRecordId, changeResponseReceivedDto.eserviceRecordId());
+    assertEquals(responseReceived, changeResponseReceivedDto.responseReceived());
+    assertEquals(status, changeResponseReceivedDto.status());
+  }
+
+}
+
