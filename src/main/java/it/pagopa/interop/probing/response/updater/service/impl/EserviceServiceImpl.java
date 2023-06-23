b634e23edb278577304c904a723056191de775bc
gdellorusso <giuseppe.dellorusso@dxc.com>
2023-06-23 09:23:14 +0200

    Xray implementation #4

--- src/main/java/it/pagopa/interop/probing/response/updater/service/impl/EserviceServiceImpl.java
+++ src/main/java/it/pagopa/interop/probing/response/updater/service/impl/EserviceServiceImpl.java
@@ -2,6 +2,7 @@ package it.pagopa.interop.probing.response.updater.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.amazonaws.xray.spring.aop.XRayEnabled;
 import it.pagopa.interop.probing.response.updater.client.EserviceClient;
 import it.pagopa.interop.probing.response.updater.dto.impl.ChangeResponseReceived;
 import it.pagopa.interop.probing.response.updater.exception.EserviceNotFoundException;
@@ -9,6 +10,7 @@ import it.pagopa.interop.probing.response.updater.service.EserviceService;
 import it.pagopa.interop.probing.response.updater.util.logging.Logger;
 
 @Service
+@XRayEnabled
 public class EserviceServiceImpl implements EserviceService {
 
   @Autowired
