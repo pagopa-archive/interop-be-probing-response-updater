93852f590043c8beda8f5d52f0ed19c2d52a2dae
gdellorusso <giuseppe.dellorusso@dxc.com>
2023-06-26 16:00:22 +0200

    Suppressed sqs errors for xray segments

---
+++ src/main/java/it/pagopa/interop/probing/response/updater/config/sqs/SqsConfig.java
@@ -0,0 +1,26 @@
+package it.pagopa.interop.probing.response.updater.config.sqs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import com.amazonaws.xray.AWSXRay;
+import com.amazonaws.xray.handlers.TracingHandler;
+import com.amazonaws.xray.strategy.IgnoreErrorContextMissingStrategy;
+
+@Configuration
+public class SqsConfig {
+
+
+  @Bean
+  @Primary
+  public AmazonSQSAsync amazonSQSAsync() {
+    AWSXRay.getGlobalRecorder().setContextMissingStrategy(new IgnoreErrorContextMissingStrategy());
+    return AmazonSQSAsyncClientBuilder.standard()
+        .withCredentials(new DefaultAWSCredentialsProviderChain())
+        .withRequestHandlers(new TracingHandler(AWSXRay.getGlobalRecorder())).build();
+  }
+
+}
