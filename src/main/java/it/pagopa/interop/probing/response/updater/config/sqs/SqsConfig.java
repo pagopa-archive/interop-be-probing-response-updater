af30c8664a479bd3e0a5bf8bfe1723605f658f72
gdellorusso <giuseppe.dellorusso@dxc.com>
2023-07-06 12:38:36 +0200

    xray-troubleshooting

--- src/main/java/it/pagopa/interop/probing/response/updater/config/sqs/SqsConfig.java
+++ src/main/java/it/pagopa/interop/probing/response/updater/config/sqs/SqsConfig.java
@@ -8,7 +8,6 @@ import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
 import com.amazonaws.xray.AWSXRay;
 import com.amazonaws.xray.handlers.TracingHandler;
-import com.amazonaws.xray.strategy.IgnoreErrorContextMissingStrategy;
 
 @Configuration
 public class SqsConfig {
@@ -17,7 +16,8 @@ public class SqsConfig {
   @Bean
   @Primary
   public AmazonSQSAsync amazonSQSAsync() {
-    AWSXRay.getGlobalRecorder().setContextMissingStrategy(new IgnoreErrorContextMissingStrategy());
+    // AWSXRay.getGlobalRecorder().setContextMissingStrategy(new
+    // IgnoreErrorContextMissingStrategy());
     return AmazonSQSAsyncClientBuilder.standard()
         .withCredentials(new DefaultAWSCredentialsProviderChain())
         .withRequestHandlers(new TracingHandler(AWSXRay.getGlobalRecorder())).build();
